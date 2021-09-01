4e787ee1203a40aad1d465a0478cbe2a23eeae85
dan.kim <ekdms33429@gmail.com>
2021-09-01 17:14:35 +0900

    commit

--- tests/test_first.py
+++ tests/test_first.py
@@ -3,18 +3,27 @@ import cv2
 import numpy as np
 from appium.webdriver.common.touch_action import TouchAction
 from selenium.common.exceptions import NoSuchElementException
-
-from desired_capabilities import android_driver
+from appium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+SERVER_URL_BASE = 'http://localhost:4723/wd/hub'
 
 class TestFirst(unittest.TestCase):
-    @classmethod
-    def setUpClass(self):
-        self.driver = android_driver()
+    def setUp(self):
+        desired_caps = {
+            "deviceName": "Galaxy Note10+",
+            "deviceUDID": "R3CM70H0BAV",
+            "app": "C:\\Users\\KIWIPLUS\\Desktop\\apk\\kiwiplay-v1.1.2(112)-stage-debug.apk",
+            'platformName': 'Android',
+            'appPackage': 'io.kiwiplus.app.kiwiplay.stage',
+            'noReset': 'true',
+            'fullReset': 'false'
+        }
+        self.driver = webdriver.Remote(SERVER_URL_BASE, desired_caps)
+        self.driver.implicitly_wait(30)
 
-    def test_first(self):
+    def test_title(self):
         id_field = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ScrollView/android.view.ViewGroup/android.widget.EditText[1]")
         pw_field = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ScrollView/android.view.ViewGroup/android.widget.EditText[2]")
         fnd_pw_Btn = self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ScrollView/android.view.ViewGroup/android.widget.TextView[1]")
@@ -31,6 +40,7 @@ class TestFirst(unittest.TestCase):
 
     def test_kakaobtn(self):
         thr = 0.95
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[contains(@text, '로그인')]")))
         self.driver.save_screenshot('screencap.png')
 
         img = cv2.imread('screencap.png') #원본 이미지
@@ -39,11 +49,11 @@ class TestFirst(unittest.TestCase):
         w, h = template.shape[::-1]
 
         res = cv2.matchTemplate(imgray, template, cv2.TM_CCOEFF_NORMED)#템플릿 매칭 함수 matchTemplate(원본 이미지, 템플릿 이미지, 템플릿 매칭 플래그) / 이미지는 8비트의 단일 채널 이미지 사용
-        loc = np.where(res >= thr) # (array_row, array_column) row = y, column = x
+        loc = np.where(res >= thr) #(array_row, array_column) row = y, column = x
 
-        for pt in zip(*loc[::-1]):
-            cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
         try:
+            for pt in zip(*loc[::-1]):
+                cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
             TouchAction(self.driver).tap(x=pt[0] + w / 2, y=pt[1] + h / 2).perform()
             WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[contains(@text, '카카오톡으로 간편로그인')]")))
         except NoSuchElementException:
